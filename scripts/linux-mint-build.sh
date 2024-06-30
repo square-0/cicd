c9d7a06e8eed1b8f2993b452d1d9b7375e7a1875
Square Zero <square-zero@outlook.com>
2024-06-30 17:53:32 -0400

    Add clean step

--- scripts/linux-mint-build.sh
+++ scripts/linux-mint-build.sh
@@ -5,6 +5,13 @@
 pushd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/.." > /dev/null
 
 
+# Clean workspace.
+rm -fr build
+mkdir build
+rm -fr dist
+mkdir dist
+
+
 # Build executable.
 python3 -OO -m PyInstaller \
     --noconfirm \
